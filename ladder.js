8b0dceff7b2ddeb68e071566f331013f19281d38
maestro92 <marty92@gmail.com>
2018-01-15 00:49:08 -0500

    playing ranking taking shape

--- ladder.js
+++ ladder.js
@@ -2,6 +2,14 @@
 // test cases: duplicate users
 // edit user name, has to change all occurences in match history as well
 
+myCurPlayerList = [];
+
+RANKING_COL_WIDTH = 50;
+NAME_COL_WIDTH = 175;
+RATING_COL_WIDTH = 175;
+
+
+INDEX_TO_COL_HEADER_MAPPING = ["#", "", "Name", "Rating", ""];
 
 
 // tabs
@@ -54,7 +62,7 @@ function ResetPagesUI()
 
 function isValidPlayerName()
 {
-    
+
 }
 
 
@@ -91,15 +99,30 @@ function savePlayerToFirebase(playerName, elo)
 
 function saveMatchHistoryToFirebase(date, playerNameA, scoreA, playerNameB, scoreB, winner)
 {
-    var playerNodeRef = bhgPingPongLadderDB.child("matches");
-    playerNodeRef.set({
+//    var playerNodeRef = bhgPingPongLadderDB.child("matches");
+//    playerNodeRef.set({
+    playerMatchesDBref.push(
+    {
         date: date,
         playerA: playerNameA,
         scoreA: scoreA,
         playerB: playerNameB,
         scoreB: scoreB,
         winner: winner
-    });
+    }, 
+
+    function(error)
+    {
+        if(error)
+        {
+            alert("Data could not be saved." + error);
+        }
+        else
+        {
+            alert("Data saved successfully.");
+        }
+    }
+    );
 }
 
 
@@ -107,23 +130,179 @@ function refreshPlayerUI(list)
 {
     var lis = '';
 
+    // sortList
+    list.sort(function(a, b)
+    {
+        return b.eloRating - a.eloRating;
+    })
+
+    /*
     // <li> tag is used in ordered lists (<ol>), unordered lists(<ul>), and in menu lists (<menu>)
     for(var i=0; i<list.length; i++)
     {
-        lis += '<li data-key="' + list[i].key + '">' + list[i].name + '     ' + list[i].eloRating +' [' + genLinks(list[i].key, list[i].name) + ']</li>';
-    //    lis += list[i].name + '     ' + list[i].eloRating +' [' + genLinks(list[i].key, list[i].name) + ']</li>';
-    
+        lis += '<li data-key="' + list[i].key + '">' + list[i].name + '     ' + parseInt(list[i].eloRating) +' [' + genLinks(list[i].key, list[i].name) + ']</li>';
     };
-
-    // sortList
-
     document.getElementById('currentPlayerList').innerHTML = lis;
+    */
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Document_Object_Model/Traversing_an_HTML_table_with_JavaScript_and_DOM_Interfaces
+    var myTableDiv = document.getElementById("currentPlayerList_Table");
+    myTableDiv.innerHTML = "";
+
+    myTableDiv.setAttribute('class', 'playerRanking');
+
+    var table = document.createElement('TABLE');
+    table.border='0';
+
+    var tableBody = document.createElement('TBODY');
+    table.appendChild(tableBody);
+
+
+    var tr = document.createElement('TR');
+    tr.setAttribute('class', 'playerRanking_tr');
+    tableBody.appendChild(tr);
+
+    buildPlayerRankingHeaderRow(tableBody);
+
+
+    for(var i=0; i<myCurPlayerList.length; i++)
+    {
+        var playerObject = myCurPlayerList[i];
+
+        var tr = document.createElement('TR');
+        tr.setAttribute('class', 'playerRanking_tr');
+        tableBody.appendChild(tr);
+
+        // cell 1
+        var td = document.createElement('TD');
+        td.setAttribute('class', 'playerRanking_td');
+        td.width = RANKING_COL_WIDTH;
+        td.appendChild(document.createTextNode(i));
+        tr.appendChild(td);
+
+        // cell 1
+        var td = document.createElement('TD');
+        td.setAttribute('class', 'playerRanking_td');
+        td.width = RANKING_COL_WIDTH;
+        td.appendChild(document.createTextNode('^'));
+        tr.appendChild(td);
+
+        // cell 2
+        var td = document.createElement('TD');
+        td.setAttribute('class', 'playerRanking_td');
+        td.width = NAME_COL_WIDTH;
+        td.appendChild(document.createTextNode(myCurPlayerList[i].name));
+        tr.appendChild(td);
+
+        // cell 3
+        var td = document.createElement('TD');
+        td.setAttribute('class', 'playerRanking_td');
+        td.width = RATING_COL_WIDTH;
+        td.appendChild(document.createTextNode( parseInt(myCurPlayerList[i].eloRating)));
+        tr.appendChild(td);
+
+        td = buildEditAndDelLinksCell(playerObject);
+
+        tr.appendChild(td);
+
+    }
+
+
+
+
+    myTableDiv.appendChild(table);
+    console.log("things are here");
+}
+
+
+// folling alligulac 
+//  #   trend   Name        Rating      hyperlink
+function buildPlayerRankingHeaderRow(tableBody)
+{
+    var tr = document.createElement('TR');
+    tr.setAttribute('class', 'playerRanking_th');
+    tableBody.appendChild(tr);
+
+    for (var i=0; i<5; i++)
+    {
+        var td = document.createElement('TD');
+        td.setAttribute('class', 'playerRanking_th');
+        td.width = RANKING_COL_WIDTH;
+        td.appendChild(document.createTextNode(INDEX_TO_COL_HEADER_MAPPING[i]));
+        tr.appendChild(td);
+    }
+}
+
+
+function buildEditAndDelLinksCell(playerObject)
+{
+    // cell 4
+    var td = document.createElement('TD');
+    td.setAttribute('class', 'playerRanking_td');
+    td.width = RATING_COL_WIDTH;
+   
+    // create link using javascript
+    // https://stackoverflow.com/questions/4772774/how-do-i-create-a-link-using-javascript
+
+    var spacer = document.createElement('a');
+    var spacerNode = document.createTextNode("[ ");
+    td.appendChild(spacerNode);
+
+
+    var editNode = document.createElement('a');
+    var linkText = document.createTextNode("Edit");
+    editNode.appendChild(linkText);
+    editNode.title = "Edit";
+    editNode.href = "javascript:edit(\'' + 'playerObject.key' + '\',\'' + 'playerObject.playerName' + '\')";
+    td.appendChild(editNode);
+
+    spacer = document.createElement('a');
+    spacerNode = document.createTextNode(" | ");
+    td.appendChild(spacerNode);
+
+
+    var delNode = document.createElement('a');
+    linkText = document.createTextNode('Delete');
+    delNode.appendChild(linkText);
+    delNode.title = "Delete";
+    delNode.href = 'javascript:del(\'' + playerObject.key + '\',\'' + playerObject.name + '\')';
+    td.appendChild(delNode);
+
+    spacer = document.createElement('a');
+    spacerNode = document.createTextNode(" ]");
+    td.appendChild(spacerNode);
+    return td;
 }
 
 
 function getTrimmedPlayerName(rawString)
 {
     return name.trim();
+}
+
+
+function findPlayerObject(playerName)
+{
+    console.log("cur list is  " + myCurPlayerList);
+    for(i = 0; i< myCurPlayerList.length; i++)
+    {
+        var player = myCurPlayerList[i];
+
+        console.log(player["name"]);
+        console.log(player.name + " " + player.eloRating);
+    }
+
+
+    for(i = 0; i< myCurPlayerList.length; i++)
+    {
+        var player = myCurPlayerList[i];
+
+        if(player.name == playerName)
+        {
+            return player;
+        }
+    }
+    return null;
 }
 
 // TODO: optimizing here
@@ -226,13 +405,13 @@ function addNewMatchResult()
     tempList.push(playerA);
     tempList.push(playerB);
 
+    var errorMsg = "";
+
     if(playerA == playerB)
     {
-        console.log("can't have the same players against each other");
+        errorMsg = "can't have the same players against each other";
         valid = false;
     }
-
-
 
 
     // check if playerA and playerB exists
@@ -259,15 +438,15 @@ function addNewMatchResult()
             }
 
             valid = false;
-            console.log("couldn't find player '" + str + "'' in our database");
+            errorMsg = "couldn't find player '" + str + "'' in our database";
         }
     }
 
     if(valid == true) 
     {
-        if(nmhWinner != playerA && nmhWinner != playerB)
+        if(winner != playerA && winner != playerB)
         {
-            console.log("invalid winnder");    
+            errorMsg = "invalid winnder";    
             valid = false;
         }
     }
@@ -278,7 +457,13 @@ function addNewMatchResult()
 
     if(valid == true)
     {
-        saveMatchHistoryToFirebase(nmhDate, nmhPlayerA, nmhScoreA, nmhPlayerB, nmhScoreB, nmhWinner);        
+        saveMatchHistoryToFirebase(date, playerA, scoreA, playerB, scoreB, winner);   
+        nmhDate.value = '';
+        nmhPlayerA.value = '';
+        nmhScoreA.value = '';
+        nmhPlayerB.value = '';
+        nmhScoreB.value = '';
+        nmhWinner.value = '';     
     }
     else
     {
@@ -286,13 +471,30 @@ function addNewMatchResult()
     }
 
 
+    if(valid == true)
+    {
+        errorMsg = "<span style=\"color:green\">" + " added " + "</span>";
+        var label = document.getElementById('errorMessageLabel').innerHTML = errorMsg;
+    }
+    else
+    {
+        errorMsg = "<span style=\"color:red\">" + errorMsg + "</span>";
+        var label = document.getElementById('errorMessageLabel').innerHTML = errorMsg;
+    }
 
-    nmhDate.value = '';
-    nmhPlayerA.value = '';
-    nmhScoreA.value = '';
-    nmhPlayerB.value = '';
-    nmhScoreB.value = '';
-    nmhWinner.value = '';
+    playerAObject = findPlayerObject(playerA);
+    playerBObject = findPlayerObject(playerB);
+
+    console.log(playerAObject);
+    console.log(playerBObject);
+
+    var winnerIsA = false;
+    if(winner == playerA)
+    {
+        winnerIsA = true;
+    }
+
+    updateELO(playerAObject, playerBObject, winnerIsA);
 }
 
 
@@ -309,7 +511,7 @@ function refreshMatchHistoryUI(list)
     // <li> tag is used in ordered lists (<ol>), unordered lists(<ul>), and in menu lists (<menu>)
     for(var i=0; i<list.length; i++)
     {
-        lis += '<li data-key="' + list[i].key + '">' + list[i].playerA + '  ' + list[i].scoreA + " : " + list[i].scoreB + '  ' + list[i].playerB + ' ' + matchWinner + ' </li>';
+        lis += '<li data-key="' + list[i].key + '">' + list[i].playerA + '  ' + list[i].scoreA + " : " + list[i].scoreB + '  ' + list[i].playerB + ' ' + list[i].winner + ' </li>';
     };
 
     // sortList
@@ -317,8 +519,45 @@ function refreshMatchHistoryUI(list)
 }
 
 
-function updateELO(matchHistory)
+// https://gamedev.stackexchange.com/questions/55441/player-ranking-using-elo-with-more-than-two-players
+function updateELO(playerA, playerB, winnerIsA)
 {
+    var ra = playerA.eloRating;
+    var rb = playerB.eloRating;
+
+    expectedScoreA = 1 / (1 + 10^(rb - ra)/400);
+    expectedScoreB = 1 / (1 + 10^(ra - rb)/400);
+
+    if(winnerIsA)
+    {
+        playerA.eloRating = ra + 32 * (1 - expectedScoreA);
+        playerB.eloRating = rb + 32 * (0 - expectedScoreB);
+    }
+    else
+    {
+        playerA.eloRating = ra + 32 * (0 - expectedScoreA);
+        playerB.eloRating = rb + 32 * (1 - expectedScoreB);
+    }
+
+//    playerA.eloRating = playerA.eloRating.toFixed(2);
+//    playerB.eloRating = playerB.eloRating.toFixed(2);
+
+    console.log(playerA.eloRating + " " + playerB.eloRating);
+
+
+    playerDBref.child(playerA.key).update(
+        {eloRating: playerA.eloRating}
+    );
+
+    playerDBref.child(playerB.key).update(
+        {eloRating: playerB.eloRating}
+    );
 
 }
 
+
+// http://www.mysamplecode.com/2012/04/generate-html-table-using-javascript.html
+function showFullPlayerListPage()
+{
+    console.log("Show full player List page");
+}
